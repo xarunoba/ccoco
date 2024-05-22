68281e6d8d328a321688b7609953b67a5662ccf3
Jude <me@xaru.win>
2024-05-22 08:16:21 +0800

    ✨ add new command `add`
    use it to add files in ccoco.config.json via CLI

---
+++ internal/cli/add.go
@@ -0,0 +1,57 @@
+package cli
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/xarunoba/ccoco/internal/config"
+)
+
+func init() {
+	cli.AddCommand(addCmd)
+}
+
+var addCmd = &cobra.Command{
+	Use:     "add [file1 file2 ...]",
+	Aliases: []string{"a"},
+	Short:   "Add file to config",
+	Long: `Adds file/s to config.
+This will add file/s to the ccoco.config.json for ccoco to generate.
+`,
+	Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		addFiles(args)
+	},
+}
+
+func addFiles(files []string) {
+	configFile := config.GetFile()
+
+	configFile.Files = append(configFile.Files, files...)
+
+	configFile.Files = dedupeSlice(configFile.Files)
+
+	configData, err := json.Marshal(configFile.Files)
+	if err != nil {
+		log.Printf("Error marshalling default config: %v", err)
+	}
+	if err := os.WriteFile(config.FileName, configData, 0644); err != nil {
+		log.Printf("Error creating file %s: %v", config.FileName, err)
+	}
+}
+
+func dedupeSlice[T comparable](sliceList []T) []T {
+	dedupeMap := make(map[T]struct{})
+	list := []T{}
+
+	for _, slice := range sliceList {
+		if _, exists := dedupeMap[slice]; !exists {
+			dedupeMap[slice] = struct{}{}
+			list = append(list, slice)
+		}
+	}
+
+	return list
+}
